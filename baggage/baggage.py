4a3030d4b654b09c7da1e59f94dd9e4c4fbf61a1
Kye W. Shi <kwshi@hmc.edu>
2019-12-25 00:02:11 -0800

    use another naive search, but with adjacent BA strictly decreasing heuristic

--- baggage/baggage.py
+++ baggage/baggage.py
@@ -1,64 +1,84 @@
 import collections as co
+import itertools as it
 import pprint as pp
 
 
+def sublists(a, indices):
+    return (a[i:j] for i, j in zip([0] + indices, indices + [len(a)]))
+
+
+def get(s, i):
+    if i < 0 or i >= len(s):
+        return ""
+    return s[i]
+
+
+def neighbors(parent, gap):
+    for i in it.chain(range(gap - 2), range(gap + 3, len(parent) - 1)):
+        p = parent[i : i + 2]
+        before = [
+            get(parent, gap - 1) + get(parent, gap + 2),
+            get(parent, i - 1) + get(parent, i),
+            get(parent, i + 1) + get(parent, i + 2),
+        ]
+        after = [
+            get(parent, gap - 1) + p[0],
+            p[1] + get(parent, gap + 2),
+            get(parent, i - 1) + get(parent, i + 2),
+        ]
+
+        if after.count("BA") != before.count("BA") - 1:
+            continue
+
+        m, n = min(i, gap), max(i, gap)
+        left, a, mid, b, right = sublists(parent, [m, m + 2, n, n + 2])
+        child = "".join([left, b, mid, a, right])
+
+        # print(parent, child, gap, i, before, after)
+        # after = [gap_pair(child, i), pair(child, gap), pair(child, gap + 2)]
+
+        # assert after.count("BA") - before.count("BA") == child.replace("_", "").count(
+        #    "BA"
+        # ) - parent.replace("_", "").count("BA")
+
+        yield child, i
+
+
+def show(s, gap):
+    return "".join(["_" * (len(s) + 2), s[:gap], "__", s[gap:]])
+
+
+def follow(parents, child):
+    yield child
+    while child in parents:
+        child = parents[child]
+        yield child
+
+
 def search(size):
-    offset = 2 * size - 1
-    start = "_" * 2 * size + "BA" * size
-    frontier = co.deque([start])
-    gap = {start: -1}
-    prev = {}
+    start = "__" + "BA" * size
+    frontier = co.deque([(start, 0)])
     seen = {start: 0}
-    end = "_" * 2 * (size - 1) + "A" * size + "B" * size + "__"
+    parents = {}
     while frontier:
-        parent = frontier.popleft()
-        g = gap[parent]
-        s = seen[parent]
-
-        for i in range(-1, 2 * size):
-            if g - 2 < i < g + 2:
-                continue
-            m, n = offset + min(i, g), offset + max(i, g)
-            left, a, mid, b, right = (
-                parent[:m],
-                parent[m : m + 2],
-                parent[m + 2 : n],
-                parent[n : n + 2],
-                parent[n + 2 :],
-            )
-            child = left + b + mid + a + right
-
+        parent, gap = frontier.popleft()
+        # print(len(frontier), end="\r")
+        for child, g in neighbors(parent, gap):
             if child in seen:
                 continue
+            seen[child] = seen[parent] + 1
+            parents[child, g] = parent, gap
 
-            seen[child] = s + 1
-            prev[child] = parent
-            gap[child] = i
+            if seen[child] == size and g == 2 * size:
+                return list(follow(parents, (child, g)))[::-1]
 
-            if child == end:
-                steps = seen[child]
-                path = []
-                p = child
-                while p != start:
-                    path.append(p)
-                    p = prev[p]
-                path.append(p)
-                return (
-                    steps,
-                    path[::-1],
-                    [gap[p] for p in reversed(path)],
-                )
-
-            if seen[child] == size:
-                continue
-
-            frontier.append(child)
+            frontier.append((child, g))
 
 
-for size in range(12, 20):
-    count, path, gaps = search(size)
-    print("n = {}: {} steps".format(size, count))
-    print(path[0])
-    for p, i, j in zip(path[1:], gaps[1:], gaps):
-        print("{}: {} to {}".format(p, i, j))
+for size in range(4, 16):
+    print("n={}:".format(size))
+    path = search(size)
+    print(path[0][0])
+    for (_, gap), (child, fill) in zip(path, path[1:]):
+        print("{}: {} to {}".format(child, fill, gap))
     print()
