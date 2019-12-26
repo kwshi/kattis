64b8d97337da6ff30ec20076c40df38390c87c9f
Kye W. Shi <kwshi@hmc.edu>
2019-12-25 16:31:13 -0800

    made some attempts

--- baggage/baggage.py
+++ baggage/baggage.py
@@ -13,8 +13,8 @@ def get(s, i):
     return s[i]
 
 
-def neighbors(parent, gap):
-    for i in it.chain(range(gap - 2), range(gap + 3, len(parent) - 1)):
+def all_neighbors(parent, gap):
+    for i in list(it.chain(range(gap - 2), range(gap + 3, len(parent) - 1)))[::1]:
         p = parent[i : i + 2]
         before = [
             get(parent, gap - 1) + get(parent, gap + 2),
@@ -27,9 +27,6 @@ def neighbors(parent, gap):
             get(parent, i - 1) + get(parent, i + 2),
         ]
 
-        if after.count("BA") != before.count("BA") - 1:
-            continue
-
         m, n = min(i, gap), max(i, gap)
         left, a, mid, b, right = sublists(parent, [m, m + 2, n, n + 2])
         child = "".join([left, b, mid, a, right])
@@ -41,7 +38,15 @@ def neighbors(parent, gap):
         #    "BA"
         # ) - parent.replace("_", "").count("BA")
 
-        yield child, i
+        yield child, i, after.count("BA") - before.count("BA")
+
+
+def decreasing_neighbors(parent, gap):
+    return ((child, i) for child, i, delta in all_neighbors(parent, gap) if delta < 0)
+
+
+def increasing_neighbors(parent, gap):
+    return ((child, i) for child, i, delta in all_neighbors(parent, gap) if delta > 0)
 
 
 def show(s, gap):
@@ -75,9 +80,53 @@ def search(size):
             frontier.append((child, g))
 
 
-for size in range(4, 16):
+def bisearch(size):
+    start = "__" + "BA" * size
+    end = "A" * size + "B" * size + "__"
+    forward = co.deque([(start, 0)])
+    backward = co.deque([(end, 2 * size)])
+    forward_seen = {start: 0}
+    backward_seen = {end: 0}
+    forward_parents = {}
+    backward_parents = {}
+    while forward and backward:
+        parent, gap = forward.popleft()
+        # print(parent)
+        for child, g in decreasing_neighbors(parent, gap):
+            # print(child)
+            if child in forward_seen:
+                continue
+            forward_seen[child] = forward_seen[parent] + 1
+            forward_parents[child, g] = parent, gap
+
+            if child in backward_seen:
+                return list(follow(forward_parents, (child, g)))[:0:-1] + list(
+                    follow(backward_parents, (child, g))
+                )
+
+            forward.append((child, g))
+
+        # print()
+        parent, gap = backward.popleft()
+        # print(parent, gap, list(increasing_neighbors(parent, gap)))
+        for child, g in increasing_neighbors(parent, gap):
+            # print(child)
+            if child in backward_seen:
+                continue
+            backward_seen[child] = backward_seen[parent] + 1
+            backward_parents[child, g] = parent, gap
+
+            if child in forward_seen:
+                return list(follow(forward_parents, (child, g)))[:0:-1] + list(
+                    follow(backward_parents, (child, g))
+                )
+
+            backward.append((child, g))
+
+
+for size in range(4, 17):
     print("n={}:".format(size))
-    path = search(size)
+    path = bisearch(size)
     print(path[0][0])
     for (_, gap), (child, fill) in zip(path, path[1:]):
         print("{}: {} to {}".format(child, fill, gap))
