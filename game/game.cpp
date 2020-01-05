d58be448fdcc65a8845bfb7087bb490d120ea08d
Kye W. Shi <kwshi@hmc.edu>
2020-01-04 20:38:05 -0800

    i do not know why i am hitting a memory limit

--- game/game.cpp
+++ game/game.cpp
@@ -10,158 +10,172 @@
 #include <limits>
 
 using namespace std;
-using opt_t = unordered_set<char>;
-using options_t = vector<vector<opt_t>>;
-using parents_t = vector<unordered_set<char>>;
-using dist_t = vector<vector<int>>;
 
-bool operator<=(const opt_t& a, const opt_t& b) {
-  if (a.size() > b.size()) return false;
-  for (char c : a)
-    if (b.find(c) == b.end()) return false;
-  return true;
-}
+//bool operator<=(const opt_t& a, const opt_t& b) {
+//  if (a.size() > b.size()) return false;
+//  for (char c : a)
+//    if (b.find(c) == b.end()) return false;
+//  return true;
+//}
 
 // filters away any option that is a superset of another
-vector<opt_t> filter_supsets(const vector<opt_t>& opts) {
-  vector<bool> keep(opts.size(), true);
-  for (size_t i{}; i < opts.size(); ++i) {
-    const opt_t& opt{opts[i]};
+//vector<opt_t> filter_supsets(const vector<opt_t>& opts) {
+//  vector<bool> keep(opts.size(), true);
+//  for (size_t i{}; i < opts.size(); ++i) {
+//    const opt_t& opt{opts[i]};
+//
+//    // check if any other option is a subset
+//    for (size_t j{}; j < opts.size(); ++j) {
+//      const opt_t& other{opts[j]};
+//
+//      if (i == j || !(other <= opt)) continue;
+//
+//      // if two options are equal, keep the earlier one
+//      if (other.size() == opt.size() && i < j) continue;
+//
+//      keep[i] = false;
+//    }
+//  }
+//
+//  vector<opt_t> kept;
+//  for (size_t i{}; i < opts.size(); ++i)
+//    if (keep[i]) kept.push_back(opts[i]);
+//  return kept;
+//}
 
-    // check if any other option is a subset
-    for (size_t j{}; j < opts.size(); ++j) {
-      const opt_t& other{opts[j]};
+char show(char c) {
+  return c + 'a';
+}
 
-      if (i == j || !(other <= opt)) continue;
-
-      // if two options are equal, keep the earlier one
-      if (other.size() == opt.size() && i < j) continue;
-
-      keep[i] = false;
-    }
-  }
-
-  vector<opt_t> kept;
-  for (size_t i{}; i < opts.size(); ++i)
-    if (keep[i]) kept.push_back(opts[i]);
-  return kept;
+string show(string s) {
+  for (char& c : s) c += 'a';
+  return s;
 }
 
 struct game {
+
   // number of nodes
-  int n;
+  size_t n;
 
-  // maps each node to its options
-  options_t options;
+  // mapping from option to set of nodes
+  vector<unordered_set<char>> contents;
 
-  // maps each node to all the nodes that point to it
-  parents_t parents;
+  // mapping from node to options containing that node
+  vector<unordered_set<size_t>> containers;
+
+  // mapping from option to nodes providing that option
+  vector<unordered_set<char>> providers;
+
+  // mapping from node to options provided by that node
+  //vector<unordered_set<size_t>> available;
+
+  // mapping from option index to string
+  //vector<string> options;
+
+  // mapping from string to index
+  //unordered_map<string, size_t> index;
 
   // parsing
   game(istream& cin) {
     cin >> n;
-    options.resize(n);
-    parents.resize(n);
+    containers.resize(n);
+    //available.resize(n);
 
-    char parent{};
-    for (vector<opt_t>& opts : options) {
-      size_t m; cin >> m;
-      opts.resize(m);
+    size_t idx{};
 
-      for (opt_t& opt : opts) {
-        string s; cin >> s;
+    for (size_t i{}; i < n; ++i) {
+      size_t m{};
+      cin >> m;
 
-        for (char c : s) {
-          c -= 'a';
-          opt.insert(c);
-          parents[c].insert(parent);
+      for (size_t j{}; j < m; ++j) {
+        string opt;
+        cin >> opt;
+
+        //if (index.find(opt) == index.end()) {
+        //  index[opt] = index.size();
+        //  //options.push_back(opt);
+        //  contents.emplace_back();
+        //  providers.emplace_back();
+        //  if (contents.size() != providers.size() || contents.size() != index.size())
+        //    throw invalid_argument{"aaa"};
+        //}
+        //size_t idx{index[opt]};
+
+        contents.emplace_back();
+        providers.emplace_back();
+
+        for (char& c : opt) c -= 'a';
+
+        //cout << contents.size() << " " << idx << endl;
+
+        for (char c : opt) {
+          containers[c].insert(idx);
+          contents[idx].insert(c);
         }
+
+        providers[idx].insert(i);
+        //available[i].insert(idx);
+
+        ++idx;
       }
-
-      auto filtered{filter_supsets(opts)};
-      opts.swap(filtered);
-
-      ++parent;
     }
+
+    //for (size_t i{}; i < options.size(); ++i) {
+    //  cout << i << " " << options[i] << endl;
+    //}
+    //cout << endl;
+    //for (auto p : index) {
+    //  cout << p.first << " " << p.second << endl;
+    //}
   }
 
-  // reverse BFS: minimum distance from each node to a fixed target
-  vector<int> rev_search(int tgt) const {
-    // We copy the options and mutate it: when a path is found from
-    // some node to the target, we remove that node from all options
-    // pointing to it.  If an option becomes empty, then all nodes
-    // originally belonging to that option have been "resolved", and
-    // the worst-case distance from that option to the target node is
-    // the distance of the last node removed from that option.  When
-    // that happens, the node referring to that option is also
-    // resolved (Alice may simply choose that option, ignoring the
-    // rest), so we remove that node from parent sets for other
-    // unresolved nodes.
-    options_t simp_options{options};
-    parents_t simp_parents{parents};
+  vector<int> propagate(char tgt) const {
+    auto remaining{contents};
 
-    deque<int> front{tgt};
+    deque<char> resolved{tgt};
+    vector<int> dist(n, -1);
 
-    // maps each node to its distance to target
-    vector<int> seen(n, -1);
-    seen[tgt] = 0;
+    dist[tgt] = 0;
 
-    for (const opt_t& opt : simp_options[tgt]) {
-      for (char child : opt) {
-        //if (simp_parents[child].find(tgt) == simp_parents[child].end()) continue;
-        simp_parents[child].erase(tgt);
+    while (!resolved.empty()) {
+      char res{resolved.front()};
+      //cout << "resolve " << static_cast<char>(res + 'a') << " " << dist[res] << endl;
+      resolved.pop_front();
+      int d{dist[res]};
+
+      // resolve res by removing it from all options containing it
+      for (size_t opt : containers[res]) {
+        //cout << "container " << show(opt) << endl;
+        remaining[opt].erase(res);
+
+        // if option is not empty, do nothing; otherwise resolve all
+        // nodes providing that option
+        if (!remaining[opt].empty()) continue;
+
+        for (char c : providers[opt]) {
+          // node is already resolved
+          if (dist[c] != -1) continue;
+
+          dist[c] = d+1;
+          //for (const string& other : options[c]) {
+          //  if (other == opt) continue;
+          //  providers[other].erase(c);
+          //}
+          resolved.push_back(c);
+        }
       }
     }
-
-    while (!front.empty()) {
-      int child{front.front()};
-      front.pop_front();
-
-      for (char parent : simp_parents[child]) {
-        if (seen[parent] != -1) continue;
-
-        // child reaches target with a distance seen[child], so
-        // eliminate child from all options containing child
-        bool found{};
-        for (opt_t& opt : simp_options[parent]) {
-          if (opt.find(child) == opt.end()) continue;
-          opt.erase(child);
-
-          // if an option is now empty, then a path has been found
-          // from parent to target, with distance seen[child]+1.
-          if (opt.empty()) {
-            found = true;
-            break;
-          }
-        }
-
-        if (!found) continue;
-
-        // remove parent from other parent sets, to avoid redundantly
-        // traversing parent again from other nodes.
-        for (const opt_t& opt : simp_options[parent]) {
-          for (char other : opt) {
-            // avoid invalidating current iterator
-            if (other == child) continue;
-            simp_parents[other].erase(parent);
-          }
-        }
-
-        seen[parent] = seen[child] + 1;
-        front.push_back(parent);
-      }
-    }
-    return seen;
+    return dist;
   }
 
-  dist_t solve() const {
-    dist_t dist(n);
+  vector<vector<int>> solve() const {
+    vector<vector<int>> dist(n);
     for (auto& d : dist) {
       d.resize(n);
     }
 
     for (char i{}; i < n; ++i) {
-      vector<int> d{rev_search(i)};
+      vector<int> d{propagate(i)};
       for (int j{}; j < n; ++j) {
         dist[j][i] = d[j];
       }
@@ -171,7 +185,7 @@ struct game {
 
 };
 
-void show(const dist_t& dist) {
+void show(const vector<vector<int>>& dist) {
   for (const auto& row : dist) {
     for (int d : row)
       cout << d << " ";
@@ -180,5 +194,6 @@ void show(const dist_t& dist) {
 }
 
 int main() {
+  //game g{cin};
   show(game{cin}.solve());
 }
